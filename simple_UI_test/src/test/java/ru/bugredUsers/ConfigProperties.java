52260cbae8ecf3154994bb550ec234daf4f2434e
MakarovaIV <tpnaatp@gmail.com>
2021-05-05 18:03:36 +0300

    added module 'simple_UI_test' and main page fields tests

---
+++ simple_UI_test/src/test/java/ru/bugredUsers/ConfigProperties.java
@@ -0,0 +1,35 @@
+package ru.bugredUsers;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigProperties {
+    protected static FileInputStream fileInputStream;
+    protected static Properties prop;
+
+    static {
+        try {
+            //path to configuration file
+            fileInputStream = new FileInputStream("src/test/resources/config.properties");
+            prop = new Properties();
+            prop.load(fileInputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Sorry, unable to find config.properties");
+            // exception handling (not exist, etc.)
+        } finally {
+            if (fileInputStream != null)
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+    }
+
+    //return string with value from file
+    public static String getProperty(String key) {
+        return prop.getProperty(key);
+    }
+}
